ae0b5777ecb0c148e10b79c9ede66ac01b384962
hsohi2 <harsimarsohi1@gmail.com>
2022-12-12 04:04:19 -0600

    Fixed where address was not loading in cart

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,4 @@
 class Province < ApplicationRecord
   validates :province_name, :gst, :pst, :hst, presence: true
   validates :gst, :pst, :hst, numericality: true
-  has_many :address, dependent: :nullify
 end
