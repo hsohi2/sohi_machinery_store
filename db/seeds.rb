5db8114787105edcfcdc21ae8c0bbe5ca7e4e31d
hsohi2 <harsimarsohi1@gmail.com>
2022-12-16 04:39:52 -0600

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
 require "csv"
 
+Category.delete_all
 Product.delete_all
+
+filename = Rails.root.join("db/categories-2022-12-16.csv")
+Rails.logger.debug "Loading products data from csv file #{filename}"
+
+csv_data = File.read(filename)
+categories_data = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+categories_data.each do |c|
+  Category.create(
+    category_name:       c["Category_name"],
+    category_description: c["Category_description"]
+  )
+end
+
+Rails.logger.debug "Loading data from csv file #{filename} completed."
+
 
 filename = Rails.root.join("db/products-2022-11-25.csv")
 Rails.logger.debug "Loading products data from csv file #{filename}"
@@ -14,10 +31,10 @@ products_data.each do |p|
     category_id:         p["Category_Id"],
     product_name:        p["Product_Name"],
     product_description: p["Product_Description"],
-    product_price:       p["Product_Price"],
-    product_image:       p["Product_Image"],
-    product_likes:       p["Product_Likes"]
+    product_price:       p["Product_Price"]
   )
 end
 
 Rails.logger.debug "Loading data from csv file #{filename} completed."
+
+AdminUser.create!(email: 'admin@example.comm', password: 'password') if Rails.env.development?
